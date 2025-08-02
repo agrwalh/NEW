4ef4f5ace9541d12ace27d973470ef8b1cb71ad5
Harsh Agarwal <haagarwal67@gmail.com>
2025-08-02 11:40:21 +0000

    so lets do one thing i have build fully fledged e commerce website and p

--- src/components/app/pharmacy.tsx
+++ src/components/app/pharmacy.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import Image from 'next/image';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
